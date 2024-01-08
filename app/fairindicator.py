ae4aa2e3ed4f975922882b5951452ecd6831c97b
rmfranken <77491494+rmfranken@users.noreply.github.com>
2024-01-08 17:08:48 +0100

    functionized stuff

--- app/fairindicator.py
+++ app/fairindicator.py
@@ -13,19 +13,21 @@ db_password = os.environ.get("GRAPHDB_PASSWORD")
 
 graph = 'https://epfl.ch/example/finalGraph'
 softwareURI = 'https://github.com/SDSC-ORD/gimie'
+
 get_relevant_software_query = """
 PREFIX ex: <https://epfl.ch/example/>
-    CONSTRUCT {
-      ?subject ex:graph <https://epfl.ch/example/finalGraph>  .
+    CONSTRUCT {{
+      ?subject ex:graph <{graph}>  .
       ?subject ?predicate ?object .
       ?object ?p ?o .
-    } WHERE {
-        GRAPH <https://epfl.ch/example/finalGraph> {
-        {?subject ?predicate ?object .
-         filter(?subject = <https://github.com/ImagingDataCommons/dicom-microscopy-viewer> )
-        OPTIONAL { ?object ?p ?o . }
-                }}}
-"""
+    }} WHERE {{
+        GRAPH <{graph}> {{
+        {{?subject ?predicate ?object .
+         filter(?subject = <{softwareURI}> )
+        OPTIONAL {{ ?object ?p ?o . }}
+                }}}}
+""".format(softwareURI=softwareURI, graph=graph)
+
 get_suggestion_query = """
 PREFIX sh: <http://www.w3.org/ns/shacl#>
 PREFIX : <https://epfl.ch/example/>
@@ -40,42 +42,47 @@ GROUP BY ?ToAchieve
 ORDER BY ?ToAchieve
 LIMIT 1
 """
+
 #Get data from GraphDB
-# sparql = SPARQLWrapper(db_host)
-# sparql.setQuery(get_relevant_software_query)
-# sparql.setReturnFormat(TURTLE)
-# sparql.setCredentials(user=db_user, passwd=db_password)
-# results = sparql.query().convert()
+def get_data_from_graphdb(db_host, db_user, db_password):
+    sparql = SPARQLWrapper(db_host)
+    sparql.setQuery(get_relevant_software_query)
+    sparql.setReturnFormat(TURTLE)
+    sparql.setCredentials(user=db_user, passwd=db_password)
+    results = sparql.query().convert()
+    return results
 
-# #Load data into rdflib
-# data_g = rdflib.Graph()
-# data_g.parse(data=results, format="turtle")
-# data_g.serialize(destination='data.ttl', format='turtle')
-
-###TEMPORARY: Load data from file
-data_g = rdflib.Graph()
-data_g.parse(file=open("./data.ttl"), format="turtle")
-###ENDOFTEMPORARY
+#Load data into rdflib
+def load_data_into_rdflib(results):
+    data_g = rdflib.Graph()
+    data_g.parse(data=results, format="turtle")
+    return data_g
 
 #Load SHACL shapes into rdflib
-shapes_g = rdflib.Graph()
-shapes_g.parse(file=open("./app/shapes.ttl"), format="turtle")
+def load_shapes_into_rdflib(shapes_path):
+    shapes_g = rdflib.Graph()
+    shapes_g.parse(file=open(shapes_path), format="turtle")
+    return shapes_g
 
 #Run SHACL validation with pyshacl
-validation_result = pyshacl.validate(data_graph = data_g, 
+def run_shacl_validation(data_g, shapes_g):
+    validation_result = pyshacl.validate(data_graph = data_g, 
                                      shacl_graph = shapes_g, 
                                      inference = 'rdfs', 
                                      serialize_report_graph = 'turtle')
-validation_result = validation_result[1]
-# print(validation_result)
-shapes_g.parse(validation_result, format="turtle")
-shapes_g.serialize(format="turtle")
+    validation_result = validation_result[1]
+    results_g = rdflib.Graph()
+    results_g.parse(validation_result, format="turtle")
+    results_g.serialize(format="turtle")
+    return results_g
 
-#Run query on SHACL validation result to get suggestions
-result2 = shapes_g.query(get_suggestion_query)
-print(result2.serialize(format="csv"))
+def get_suggestions(results_g):
+    #Run query on SHACL validation result to get suggestions
+    result2 = results_g.query(get_suggestion_query)
+    return result2.serialize(format="json")
 
-#TODO: Run query on SHACL validation result to get suggestions
+
+
 
 
 
