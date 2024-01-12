6e4ee65796c853a7bffdf38771aef5e64cfbc7f1
rmfranken <77491494+rmfranken@users.noreply.github.com>
2024-01-12 09:34:13 +0100

    Remove useless "head" from suggestions_dict

--- imaging_plaza_fair_indicator_api/fairindicator.py
+++ imaging_plaza_fair_indicator_api/fairindicator.py
@@ -147,5 +147,8 @@ def indicate_fair(softwareURI:str, graph:str, shapesfile:str = "shapes.ttl") -> 
     suggestions = get_suggestions(results_g)
 
     suggestions_dict = json.loads(suggestions)
-
+    if "head" in suggestions_dict:
+        del suggestions_dict["head"]
+    suggestions_dict = json.dumps(suggestions_dict)
+    
     return suggestions_dict
