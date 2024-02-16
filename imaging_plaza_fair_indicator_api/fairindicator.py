84312cb5fc06e2b0ff6f1ad9ce06efe3928ad9dd
rmfranken <77491494+rmfranken@users.noreply.github.com>
2024-02-16 09:00:21 +0100

    testing if fair indications bug is coming from this branch

--- imaging_plaza_fair_indicator_api/fairindicator.py
+++ imaging_plaza_fair_indicator_api/fairindicator.py
@@ -115,7 +115,7 @@ def get_suggestions(results_g: rdflib.Graph, ontology_g: rdflib.Graph) -> str:
     PREFIX : <https://epfl.ch/example/>
     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-    select ?focusNode ?pathLabel ?ToAchieve
+    select ?focusNode ?path ?ToAchieve
     WHERE{
     
     {
