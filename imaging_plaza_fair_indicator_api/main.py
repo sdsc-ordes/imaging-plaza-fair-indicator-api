3f9220b3130961242e5c0e69e4a2e2e0df6d38a9
Robin Franken <77491494+rmfranken@users.noreply.github.com>
2025-01-20 10:46:33 +0100

    Increment version number of Fair level indicator

--- imaging_plaza_fair_indicator_api/main.py
+++ imaging_plaza_fair_indicator_api/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"title": "Hello, welcome to the Imaging Plaza Fair Level Indicator v0.0.8"}
+    return {"title": "Hello, welcome to the Imaging Plaza Fair Level Indicator v0.0.9"}
 
 @app.get("/indicate/")
 async def indicate(uri:str, graph: str):
