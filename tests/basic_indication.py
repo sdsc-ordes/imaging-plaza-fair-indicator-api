c62fa810af90813b5079ab4a668b2bab01be9f59
rmfranken <robin.franken@epfl.ch>
2025-04-09 09:29:47 +0200

    feat:update ontology to v0.9

--- tests/basic_indication.py
+++ tests/basic_indication.py
@@ -1,8 +1,9 @@
-from ..app import fairindicator
+from imaging_plaza_fair_indicator_api import fairindicator
+# print(indicate_fair('https://github.com/flatironinstitute/CaImAn', 'https://imaging-plaza.epfl.ch/finalGraph', 'ImagingOntologyCombined.ttl'))
 
-uri = "https://github.com/ImagingDataCommons/dicom-microscopy-viewer"
-graph = "temporary"
+uri = "https://github.com/flatironinstitute/CaImAn"
+graph = "https://imaging-plaza.epfl.ch/finalGraph"
 
-suggestions = fairindicator.indicate_fair(uri, graph)
+suggestions = fairindicator.indicate_fair(uri, graph, "ImagingOntologyCombined.ttl")
 
 print(suggestions)
